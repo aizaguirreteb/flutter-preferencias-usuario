3b895ea5f90b33e7594e0163333d3802774e6350
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-05-03 15:38:21 +0200

    controlando estado con los elementos del form

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:preferencias/src/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
 
   static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nombre = 'Pedro';
+
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textController = new TextEditingController(text: _nombre);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,23 +39,37 @@ class SettingsPage extends StatelessWidget {
           ),
           Divider(),
           SwitchListTile(
-            value: true,
+            value: _colorSecundario,
             title: Text('Color secundario'),
-            onChanged: ( value ){},
+            onChanged: ( value ){
+              setState(() {
+                _colorSecundario = value;                
+              });
+            },
           ),
 
           RadioListTile(
             value: 1,
             title: Text('Masculino'),
-            groupValue: 1,
-            onChanged: ( value ){},
+            groupValue: _genero,
+            onChanged: ( value ){
+              setState(() {
+                _genero = value;
+                
+              });
+            },
           ),
 
           RadioListTile(
-            value: 1,
+            value: 2,
             title: Text('Femenino'),
-            groupValue: 2,
-            onChanged: ( value ){},
+            groupValue: _genero,
+            onChanged: ( value ){
+              _genero = value;
+              setState(() {
+                _genero = value;                
+              });
+            },
           ),
 
           Divider(),
@@ -44,6 +77,7 @@ class SettingsPage extends StatelessWidget {
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: TextField(
+              controller: _textController,
               decoration: InputDecoration(
                 labelText: 'Nombre',
                 helperText: 'Nombre de la persona usando el teléfono'
